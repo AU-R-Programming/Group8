fd3e2a50936d6fff1973a9b2e78363e2f6d54fb9
lad0047 <lad0047@auburn.edu>
2020-08-28 14:27:22 -0500

    Class files

---
+++ exercise_baseR_github.R
@@ -0,0 +1,16 @@
+# 1) Generate one observation from a Poisson distribution with lambda = 5 and assign it to an object called `test`
+
+# 2) Take the square root of `test` and multiply it by the logarithm of 10
+
+# 3) Install the simts package from CRAN
+
+# 4) Generate Figure 1 in the vignette https://cran.r-project.org/web/packages/simts/vignettes/vignettes.html
+# using the code available in the vignette
+
+# 5) Install the irg package from the GitHub repository at https://github.com/SMAC-Group/irg
+
+# 6) Find help in R for the function `granger_test` (in the irg package) and run the example at the end of the help documentation
+
+# 7) Load the data class_survey.csv (available on Canvas>Files)
+
+# 8) Using the functions `barplot` and `table`, create barplots of the three variables in the loaded data. Comment.
