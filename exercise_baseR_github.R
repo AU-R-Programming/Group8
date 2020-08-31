ae8cf23ca211a203d253614c91407f6495308b5e
gvsairahul <gvsairahul@gmail.com>
2020-08-31 12:35:23 -0500

    Complete assignment

--- exercise_baseR_github.R
+++ exercise_baseR_github.R
@@ -6,7 +6,7 @@ sqrt(test)*log(10)
 install.packages('simts')
 # 4) Generate Figure 1 in the vignette https://cran.r-project.org/web/packages/simts/vignettes/vignettes.html
 # using the code available in the vignette
-library('simts')
+library(simts)
 set.seed(1337)
 n = 10^4
 wn = gen_gts(n, WN(sigma2 = 1))
@@ -17,9 +17,19 @@ plot(wn)
 plot(qn)
 plot(rw)
 # 5) Install the irg package from the GitHub repository at https://github.com/SMAC-Group/irg
-install.packages('devtools')
+library(devtools)
+install_github("SMAC-Group/irg")
+library(irg)
 # 6) Find help in R for the function `granger_test` (in the irg package) and run the example at the end of the help documentation
-
+?granger_test
+data(signals)
+times <- c(0, 5, 10, 15, 20, 30, 45, 60, 90, 120)
+granger_test(root = signals$root, shoot = signals$shoot, times = times, alternative = "twodir")
 # 7) Load the data class_survey.csv (available on Canvas>Files)
-
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+df<-read.csv("class_survey.csv")
 # 8) Using the functions `barplot` and `table`, create barplots of the three variables in the loaded data. Comment.
+attach(df)
+barplot(table(Programming.Experience))
+barplot(table(R.Experience))
+barplot(table(GitHub.Experience))
